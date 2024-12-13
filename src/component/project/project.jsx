713142b2caed9d411b261f2cbbbc592d39098780
faaanw <akuafwan2@gmail.com>
2024-12-13 08:41:42 +0700

    update3

--- src/component/project/project.jsx
+++ src/component/project/project.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import projects from "../../data/projects.json";
-import styles from "project.module.css";
-import { ProjectCard } from "ProjectCard"; // Ganti dengan huruf kapital
+import styles from "./project.module.css";
+import { ProjectCard } from "./ProjectCard"; // Ganti dengan huruf kapital
 
 export const Project = () => {
   return (
