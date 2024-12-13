87c4e694485be88310a25f62c0cee927536a8037
faaanw <akuafwan2@gmail.com>
2024-12-13 08:37:09 +0700

    update1

--- src/component/project/project.jsx
+++ src/component/project/project.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import projects from "../../data/projects.json";
-import styles from './project.module.css';
-import { ProjectCard } from "./projectCard"; // Ganti dengan huruf kapital
+import styles from "./project.module.css";
+import { ProjectCard } from "./ProjectCard"; // Ganti dengan huruf kapital
 
 export const Project = () => {
   return (
@@ -9,9 +9,7 @@ export const Project = () => {
       <h2 className={styles.title}>Projects</h2>
       <div className={styles.projects}>
         {projects.map((project, id) => {
-          return (
-            <ProjectCard key={id} project={project} /> 
-          );
+          return <ProjectCard key={id} project={project} />;
         })}
       </div>
     </section>
